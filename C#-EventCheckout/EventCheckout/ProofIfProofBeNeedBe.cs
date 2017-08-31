d1616e6d32c0200e91c7a2ef4a920c7c5f7c1a13
pauldambra <pauldambra@gmail.com>
2017-08-31 22:02:02 +0100

    can talk to eventstore and back out with a real object

--- C#-EventCheckout/EventCheckout/ProofIfProofBeNeedBe.cs
+++ C#-EventCheckout/EventCheckout/ProofIfProofBeNeedBe.cs
@@ -7,50 +7,58 @@ using Xunit;
 
 namespace EventCheckout
 {
+   
     public class ProofIfProofBeNeedBe
     {
+        private readonly IEventStoreConnection _eventStoreConnection = EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113));
+
+        private async Task WriteBytesToStream(string streamName, string eventType, string eventData)
+        {
+            var myEvent = new EventData(Guid.NewGuid(), eventType, false,
+                Encoding.UTF8.GetBytes(eventData),
+                Encoding.UTF8.GetBytes("some metadata"));
+
+            await _eventStoreConnection.AppendToStreamAsync(streamName,
+                ExpectedVersion.Any, myEvent);
+        }
+
+        private async Task WriteJSONToStream(string streamName, string eventType, string eventData)
+        {
+            var myEvent = new EventData(Guid.NewGuid(), eventType, true,
+                Encoding.UTF8.GetBytes(eventData),
+                Encoding.UTF8.GetBytes("some metadata"));
+
+            await _eventStoreConnection.AppendToStreamAsync(streamName,
+                ExpectedVersion.Any, myEvent);
+        }
+
         [Fact]
         public async Task TheExampleCanRun()
         {
-            var connection =
-                EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113));
-            await connection.ConnectAsync();
+            await _eventStoreConnection.ConnectAsync();
 
-            var myEvent = new EventData(Guid.NewGuid(), "testEvent", false,
-                Encoding.UTF8.GetBytes("some data"),
-                Encoding.UTF8.GetBytes("some metadata"));
-
-            await connection.AppendToStreamAsync("test-stream",
-                ExpectedVersion.Any, myEvent);
+            await WriteBytesToStream("test-stream", "testEvent", "some data");
 
             var streamEvents = await 
-                connection.ReadStreamEventsForwardAsync("test-stream", 0, 1, false);
+                _eventStoreConnection.ReadStreamEventsBackwardAsync("test-stream", StreamPosition.End, 1, false);
 
             var returnedEvent = streamEvents.Events[0].Event;
-
             var returnedData = Encoding.UTF8.GetString(returnedEvent.Data);
             var returnedMetaData = Encoding.UTF8.GetString(returnedEvent.Metadata);
             
             Assert.Equal("some data", returnedData);
             Assert.Equal("some metadata", returnedMetaData);
         }
-        
+
         [Fact]
         public async Task TheExampleCanRunWithJsonStored()
         {
-            var connection =
-                EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113));
-            await connection.ConnectAsync();
+            await _eventStoreConnection.ConnectAsync();
 
-            var myEvent = new EventData(Guid.NewGuid(), "testEvent", true,
-                Encoding.UTF8.GetBytes("{\"some\": \"data\"}"),
-                Encoding.UTF8.GetBytes("some metadata"));
-
-            await connection.AppendToStreamAsync("test-stream",
-                ExpectedVersion.Any, myEvent);
-
+            await WriteJSONToStream("test-stream", "testEvent", "{\"some\": \"data\"}");
+            
             var streamEvents = await 
-                connection.ReadStreamEventsBackwardAsync("test-stream", StreamPosition.End, 1, false);
+                _eventStoreConnection.ReadStreamEventsBackwardAsync("test-stream", StreamPosition.End, 1, false);
 
             var returnedEvent = streamEvents.Events[0].Event;
 
